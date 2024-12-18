39f6a4d90c689778be689d89a0c609ef9641a9fb
HP_Nam <namphg1412@gmail.com>
2024-12-19 00:37:33 +0700

    refactor xulykhachhang.php to improve code readability and maintain consistent formatting

--- php/xulykhachhang.php
+++ php/xulykhachhang.php
@@ -4,30 +4,26 @@
     if(!isset($_POST['request']) && !isset($_GET['request'])) die(null);
 
     switch ($_POST['request']) {
-    	case 'getall':
-				$khachang = (new NguoiDungBUS())->select_all();
-                
-		    	die (json_encode($khachang));
-    		break;
+        case 'getall':
+            $khachang = (new NguoiDungBUS())->select_all();
+            die (json_encode($khachang));
+            break;
 
-    	case 'changeTT':
-				$khachhangBUS = new NguoiDungBUS();
-				$key = $_POST['key'];
-				$trangthai = $_POST['trangThai'];
-				
-		    	die (json_encode($khachhangBUS->capNhapTrangThai($trangthai, $key)));
-    		break;
+        case 'changeTT':
+            $khachhangBUS = new NguoiDungBUS();
+            $key = $_POST['key'];
+            $trangthai = $_POST['trangThai'];
+            die (json_encode($khachhangBUS->capNhapTrangThai($trangthai, $key)));
+            break;
 
-	    case 'delete':
-				$khachhangBUS = new NguoiDungBUS();
-				$mand = $_POST['mand'];
-					
-			    die (json_encode($khachhangBUS->delete_by_id($mand	)));
-	    	break;
+        case 'delete':
+            $khachhangBUS = new NguoiDungBUS();
+            $mand = $_POST['mand'];
+            die (json_encode($khachhangBUS->delete_by_id($mand)));
+            break;
 
-
-	default:
-    		# code...
-    		break;
+        default:
+            # code...
+            break;
     }
 ?>
